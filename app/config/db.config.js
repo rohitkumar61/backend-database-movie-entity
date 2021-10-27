673641fc8b3cd7efaa317964481d5f7a4377c106
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-27 19:28:54 +0530

    Remove unused keys

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,11 @@
-require("dotenv").config({path:"./../../.env"})
-
+require("dotenv").config({ path: "./../../.env" });
 
 module.exports = {
-	HOST: process.env.HOST,
-	USER: process.env.USER,
-	PASSWORD: process.env.PASSWORD,
-	DB: process.env.DB,
-	dialect: "postgres",
-	pool: {
-	  max: 5,
-	  min: 0,
-	  acquire: 30000,
-	  idle: 10000
-	}
-  };
+  URL: process.env.URL,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
