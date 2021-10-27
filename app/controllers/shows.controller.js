8e47870b5f76ba62320b1129bfa63abbc49498b8
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-28 03:39:25 +0530

    Create shows

---
+++ app/controllers/shows.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const Shows = db.shows;
+
+
+exports.create = (req, res) => {
+	// Validate request
+	if (!req.body.movie_name) {
+	  res.status(400).send({
+		message: "Content can not be empty!"
+	  });
+	  return;
+	}
+  
+	
+    const id =req.params.id
+	const shows = {
+		movieId: `${id}`,
+		movie_name: req.body.movie_name,
+		theatre_name: req.body.theatre_name,
+		rating: req.body.rating,
+	    movie_info: req.body.movie_info,
+	};
+  
+	// Save Movie in the database
+	Shows.create(shows)
+	  .then(data => {
+		res.send(data);
+	  })
+	  .catch(err => {
+		res.status(500).send({
+		  message:
+			err.message || "Some error occurred while creating the Movie."
+		});
+	  });
+  };
