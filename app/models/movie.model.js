46c0c02a7b5bb18822cfc9608057574c1dcef6e5
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-28 04:05:16 +0530

    Movie model add not null

--- app/models/movie.model.js
+++ app/models/movie.model.js
@@ -1,16 +1,21 @@
 module.exports = (sequelize, Sequelize) => {
 	const Movie = sequelize.define("movie", {
+
 	  title: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		notNull: true,
 	  },
 	  director: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		notNull: true,
 	  },
 	  year: {
-		type: Sequelize.INTEGER
+		type: Sequelize.INTEGER,
+		notNull: true,
 	  },
 	  length_minutes: {
-		type: Sequelize.INTEGER
+		type: Sequelize.INTEGER,
+		notNull: true,
 	  }
 	});
   
